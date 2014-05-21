7cc3a9acbafccbb6397079dacbf7537e35cedcf6
guifre <guifre.ruiz@owasp.org>
2014-05-21 20:04:49 +0100

    interface definition for trade strategy

--- src/org/btcwolf/strategy/Strategy.java
+++ src/org/btcwolf/strategy/Strategy.java
@@ -1,7 +1,13 @@
 package org.btcwolf.strategy;
 
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by guifre on 20/05/14.
  */
 public interface Strategy {
+
+    public double run(List<Double> a) throws IOException;
+
 }
