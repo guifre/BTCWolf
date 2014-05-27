38e3f78c78476670ef03041c5956a82d8befcdd0
guifre <guifre.ruiz@owasp.org>
2014-05-27 23:07:42 +0100

    minor code cleanup

--- test/org/btcwolf/strategy/TradingStrategyTest.java
+++ test/org/btcwolf/strategy/TradingStrategyTest.java
@@ -13,6 +13,9 @@ import java.util.List;
 
 public class TradingStrategyTest {
 
+    private static final String LOG4J_PATH = "./resources/log4j.properties";
+    private static final Logger logger = Logger.getLogger(AbstractTradingStrategy.class.getName());
+
     public List<Ticker> getTicker() {
         List<Ticker> list2 = null;
         try {
@@ -25,15 +28,16 @@ public class TradingStrategyTest {
 
     @Test
     public void testStrategy() {
-        String log4jConfPath = "resources/logger.properties";
-        PropertyConfigurator.configure(log4jConfPath);
 
-        Logger logger = Logger.getLogger(AbstractTradingStrategy.class.getName());
+        PropertyConfigurator.configure(LOG4J_PATH);
+
         BigDecimal bitThreshold = BigDecimal.valueOf(5);
         BigDecimal currThreshold = BigDecimal.valueOf(3);
         BigDecimal yuan = BigDecimal.valueOf(1496); //about £1k
+
         List<Ticker> data = getTicker();
         TradingStrategy testedStrategy = new WinWinTradingStrategy(new BTCChinaAgent(), bitThreshold, currThreshold);
+
         logger.info("BTC threshold[" + String.format("%.1f", bitThreshold) +
                 "] Curr Threshold[" + String.format("%.1f", currThreshold) +
                 "] Profit [" + String.format("%.4f", ((WinWinTradingStrategy)testedStrategy).run(data)) + "]");
